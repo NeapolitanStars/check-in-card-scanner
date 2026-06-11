168ec60d325ded0588b781d503765f046e367c2d
merunea <notneapolitanstars@gmail.com>
2026-06-11 09:14:30 -0400

    added real pin numbers

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,22 @@
 #include <MFRC522.h>
 #include <SPI.h>
 #include <LiquidCrystal.h>
+#include <HTTPClient.h>
 
 // SSID and password for the ESP-32 to connect to WiFi
 char ssid[] = "neapy";
-char password[] = "neapolitan";
+char password[] = "neapolitan"; // Yes you may have my hotspot password
 
 // Initialize pins
 int led_pin = 2;
-int ss_pin = 4;  // dummy value
-int rst_pin = 3; // dummy value
+int ss_pin = 19;  // For card reader
+int rst_pin = 15; // For card reader
 
 // Initialize LCD screen
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // dummy value
+LiquidCrystal lcd(26, 25, 33, 32, 35, 34); // RS, E, D4, D5, D6, D7
 
 // Initialize mfrc522, which allows reading/writing to RFID tags
-MFRC522 mfrc522(ss_pin, rst_pin);
+MFRC522 rfid(ss_pin, rst_pin);
 
 void setup()
 {
@@ -26,8 +27,9 @@ void setup()
   lcd.begin(16, 2);
   Serial.begin(9600);
   SPI.begin(); // Initialize Serial Peripheral Interface (SPI), which allows the ESP-32 to communicate with the NFID reader
+  rfid.PCD_Init(); // Initialize the RFID reader
 
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password); // Connects the ESP-32 to Wi-Fi
   Serial.println("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -41,4 +43,21 @@ void setup()
 
 void loop()
 {
+  // Wait for a card to be present on the reader
+  if(!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()){
+    return;
+  }
+
+  // Read the card's UID
+  String cardID = "";
+  for (byte i = 0; i < rfid.uid.size; i++){
+    cardID += String(rfid.uid.uidByte[i], HEX);
+  }
+  cardID.toUpperCase();
+
+  Serial.println("Card ID: " + cardID);
+
+  
+  // Send card ID to Google Apps Script
+  HTTPClient http;
 }
